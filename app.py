9b8da65fcb5a92bbf3b2f49d2867b3a26a5792bb
Manoj <163501471+manojch77@users.noreply.github.com>
2025-06-07 17:22:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def receive_data():
     return "Missing data", 400
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5500, debug=True)
+    app.run(host="0.0.0.0", port=5500, debug=True, threaded=True)
